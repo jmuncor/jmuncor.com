7affc821a72d7b78b88a7497e3c64ccd60cdc303
Juan Munoz Corredor <jsmucor@gmail.com>
2026-01-11 21:23:43 -0800

    Add robots.txt and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://jmuncor.com',
 	integrations: [mdx(), sitemap()],
 });
