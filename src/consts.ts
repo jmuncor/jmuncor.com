e8fde4dcac0df11241a1a6fea3cbe0de4690f59c
Juan Munoz Corredor <jsmucor@gmail.com>
2026-01-11 20:15:02 -0800

    Changed my personal description in the website

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'jmuncor';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Industrial Engineer, AI enthusiast, and GTM specialist sharing thoughts on startups and technology.';
